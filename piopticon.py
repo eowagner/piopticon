82b30ac68aba52bc39f14992928cc8c8c7128434
eowagner <e.o.wagner@gmail.com>
2016-05-30 22:51:51 +0000

    works to detect motion and paint rectangles

--- piopticon.py
+++ piopticon.py
@@ -8,7 +8,7 @@ import time
 
 min_upload_seconds = 3.0
 min_motion_frames = 8
-camera_warmup_time = 2
+camera_warmup_time = .2
 delta_thresh = 5
 min_area = 5000
 
@@ -33,22 +33,25 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 
     if avg is None:
         print "Starting background model"
+        #avg = gray
         avg = gray.copy().astype("float")
         rawCapture.truncate(0)
         continue
 
     cv2.accumulateWeighted(gray, avg, 0.5)
     frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+    #frameDelta = cv2.absdiff(avg, gray)    
 
     thresh = cv2.threshold(frameDelta, delta_thresh, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=2)
 
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    
 
     for c in cnts:
         if cv2.contourArea(c) < min_area:
-            continue
-
+            continue	
         (x, y, w, h) = cv2.boundingRect(c)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         text = "Occupied"
