02788a642191e37ea5f1a630e78f467d52ae796d
Elliott Wagner <e.o.wagner@gmail.com>
2016-06-01 15:39:07 -0500

    Now texts when there is a dropbox error
    More parameters in json file

--- piopticon.py
+++ piopticon.py
@@ -19,25 +19,16 @@ from dropbox.exceptions import ApiError, AuthError
 from twilio.rest import TwilioRestClient
 
 
-local_dir = "/home/pi/code/piopticon/" # Should probably get by system call -- no relative addresses work if the prog starts on boot
+local_dir = "/home/pi/code/piopticon/" # Should probably get by system call--relative addresses do not work if the prog starts on boot
 
 conf = json.load(open(local_dir + "config.json"))
-
-min_text_seconds = conf["min_text_seconds"]
-min_upload_seconds = conf["min_upload_seconds"]
-min_motion_frames = conf["min_motion_frames"]
-camera_warmup_time = conf["camera_warmup_time"]
-delta_thresh = conf["delta_thresh"]
-min_area = conf["min_area"] # 10% of the size of the frame on which processing is done seems to work well
-resolution = tuple(conf["resolution"]) # (640, 480) works with fps=16, but I think there is weird downscaling going on
-fps = conf["fps"]
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-showvideo', action="store_true", default=False)
 args = vars(parser.parse_args())
 
 print "Waiting for wifi..."
-time.sleep(10)
+time.sleep(conf['start_delay'])
 
 client = TwilioRestClient(conf["twilio_sid"], conf["twilio_token"])
 
@@ -48,11 +39,11 @@ except AuthError as err:
     sys.exit("ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
 
 camera = PiCamera()
-camera.resolution = resolution
-camera.framerate = fps
-rawCapture = PiRGBArray(camera, size=resolution)
+camera.resolution = tuple(conf["resolution"])
+camera.framerate = conf["fps"]
+rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
 
-time.sleep(camera_warmup_time)
+time.sleep(conf["camera_warmup_time"])
 
 avg = None
 lastUploaded = datetime.datetime.now()
@@ -66,7 +57,7 @@ try:
         timestamp = datetime.datetime.now()
         motion = False
 
-        frame = imutils.resize(frame, width=500)
+        frame = imutils.resize(frame, width=conf["resize_to"])
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -81,13 +72,13 @@ try:
         frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
         #frameDelta = cv2.absdiff(avg, gray)
 
-        thresh = cv2.threshold(frameDelta, delta_thresh, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.threshold(frameDelta, conf['delta_thresh'], 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations=2)
 
-        (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        (_, contours, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        for c in cnts:
-            if cv2.contourArea(c) < min_area:
+        for c in contours:
+            if cv2.contourArea(c) < conf['min_contour_area']:
                 continue
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
@@ -97,23 +88,22 @@ try:
         cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
         if motion:
-            if motionCounter >= min_motion_frames:
+            if motionCounter >= conf['min_motion_frames']:
                 motionCounter = 0
 
-                if (timestamp - lastTexted).seconds >= min_text_seconds:
+                if (timestamp - lastTexted).seconds >= conf['min_text_seconds']:
                     client.messages.create(
-                        to="4404768415",
-                        from_="+12164506265",
-                        body="Motion Detected"
+                        to = conf["destination_number"],
+                        from_= conf["origin_number"],
+                        body = "Piopticon Detected Motion"
                     )
                     lastTexted = timestamp
 
-                if (timestamp - lastUploaded).seconds >= min_upload_seconds:
+                if (timestamp - lastUploaded).seconds >= conf['min_upload_seconds']:
                     lastUploaded = timestamp
 
                     name = "{}.jpg".format(timestamp.strftime("%I:%M:%S%p"))
                     localName = local_dir+name
-                    # dbxName = "/"+localName
                     dbxName = "/{}/{}".format(timestamp.strftime("%Y-%B-%d"), name)
                     cv2.imwrite(localName, frame)
 
@@ -124,16 +114,12 @@ try:
                         try:
                             dbx.files_upload(f, dbxName, mode=WriteMode('overwrite'))
                         except ApiError as err:
-                            # This checks for the specific error where a user doesn't have
-                            # enough Dropbox space quota to upload this file
-                            if err.error.is_path() and err.error.get_path().error.is_insufficient_space():
-                                sys.exit("ERROR: Cannot back up; insufficient space.")
-                            elif err.user_message_text:
-                                print(err.user_message_text)
-                                sys.exit()
-                            else:
-                                print(err)
-                                sys.exit()
+                            client.messages.create(
+                                to = conf["destination_number"],
+                                from_= conf["origin_number"],
+                                body = "Piopticon Dropbox Error!"
+                             )
+                            sys.exit()
 
                     os.remove(localName)
 
